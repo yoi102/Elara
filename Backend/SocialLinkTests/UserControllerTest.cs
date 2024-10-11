8fa6d011ad8c81b7b4d90c3e9929f342cbca2fb9
yoiri <yoiri102@gmail.com>
2024-10-12 02:13:02 +0900

    UserControllerTest

--- Backend/SocialLinkTests/UserControllerTest.cs
+++ Backend/SocialLinkTests/UserControllerTest.cs
@@ -7,24 +7,29 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using SocialLink.Domain.Entities;
 using SocialLink.Domain.Interfaces;
+using SocialLink.Domain.Results;
 using SocialLink.WebAPI.Controllers.User;
+using SocialLink.WebAPI.Controllers.User.Request;
+using SocialLink.WebAPI.Controllers.User.Response;
+using SocialLink.WebAPI.Events;
+using System.Net;
 using System.Security.Claims;
 
 namespace SocialLinkTests
 {
     public class UserControllerTest
     {
-        private Mock<IEventBus> eventBus;
+        private Mock<IEventBus> eventBusMock;
         private UserController userController;
         private Mock<IUserDomainService> userDomainServiceMock;
-        private Mock<IUserRepository> userRepository;
+        private Mock<IUserRepository> userRepositoryMock;
         public UserControllerTest()
         {
             userDomainServiceMock = new Mock<IUserDomainService>();
-            userRepository = new Mock<IUserRepository>();
-            eventBus = new Mock<IEventBus>();
+            userRepositoryMock = new Mock<IUserRepository>();
+            eventBusMock = new Mock<IEventBus>();
             userController = new UserController(userDomainServiceMock.Object,
-                                       userRepository.Object, eventBus.Object);
+                                       userRepositoryMock.Object, eventBusMock.Object);
         }
 
         [Fact]
@@ -41,7 +46,7 @@ namespace SocialLinkTests
                 HttpContext = new DefaultHttpContext { User = user }
             };
 
-            userRepository.Setup(repo => repo.RemoveUserAsync(It.IsAny<UserId>()))
+            userRepositoryMock.Setup(repo => repo.RemoveUserAsync(It.IsAny<UserId>()))
                                  .ReturnsAsync(IdentityResult.Failed())
                                  .Verifiable();
 
@@ -50,16 +55,15 @@ namespace SocialLinkTests
             var badRequestResult = Assert.IsType<BadRequestResult>(result);
             badRequestResult.StatusCode.Should().Be(400);
 
-            userRepository.Verify(repo => repo.RemoveUserAsync(It.Is<UserId>(id => id == UserId.Parse(userId.ToString()))), Times.Once);
+            userRepositoryMock.Verify(repo => repo.RemoveUserAsync(It.Is<UserId>(id => id.Value == userId)), Times.Once);
         }
 
         [Fact]
         public async Task Delete_ReturnsNotFound_WhenUserIdIsNullOrEmpty()
         {
-            var userId = Guid.NewGuid();
             var user = new ClaimsPrincipal(new ClaimsIdentity(
             [
-            new Claim(ClaimTypes.Name, userId.ToString())
+            new Claim(ClaimTypes.NameIdentifier, "")
             ]));
 
             userController.ControllerContext = new ControllerContext
@@ -87,7 +91,7 @@ namespace SocialLinkTests
                 HttpContext = new DefaultHttpContext { User = user }
             };
 
-            userRepository.Setup(repo => repo.RemoveUserAsync(It.IsAny<UserId>()))
+            userRepositoryMock.Setup(repo => repo.RemoveUserAsync(It.IsAny<UserId>()))
                                  .ReturnsAsync(IdentityResult.Success)
                                  .Verifiable();
 
@@ -96,7 +100,312 @@ namespace SocialLinkTests
             var okResult = Assert.IsType<OkResult>(result);
             okResult.StatusCode.Should().Be(200);
 
-            userRepository.Verify(repo => repo.RemoveUserAsync(It.Is<UserId>(id => id == UserId.Parse(userId.ToString()))), Times.Once);
+            userRepositoryMock.Verify(repo => repo.RemoveUserAsync(It.Is<UserId>(id => id == UserId.Parse(userId.ToString()))), Times.Once);
         }
+
+        [Fact]
+        public async Task GetEmailResetCode_ReturnsOk_WhenUserIsFound()
+        {
+            var email = "123@123.com";
+            GetEmailResetCodeResult getEmailResetCodeResult =
+                new GetEmailResetCodeResult(IdentityResult.Success, email, "Subject", "Message");
+            ResetPasswordByEmailResetCodeEvent resetPasswordByEmailResetCodeEvent =
+                new ResetPasswordByEmailResetCodeEvent(getEmailResetCodeResult.Email, getEmailResetCodeResult.Subject, getEmailResetCodeResult.HtmlMessage);
+            userDomainServiceMock.Setup(ud => ud.GetEmailResetCode(It.IsAny<string>())).ReturnsAsync(getEmailResetCodeResult);
+            eventBusMock.Setup(eb => eb.Publish(It.IsAny<string>(), It.IsAny<ResetPasswordByEmailResetCodeEvent>()));
+
+
+            var result = await userController.GetEmailResetCode(email);
+
+
+            userDomainServiceMock.Verify(ud => ud.GetEmailResetCode(email));
+            eventBusMock.Verify(eb => eb.Publish("UserService.User.ResetUserPasswordByEmail", resetPasswordByEmailResetCodeEvent));
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            okResult.StatusCode.Should().Be(200);
+
+        }
+
+        [Fact]
+        public async Task GetEmailResetCode_ReturnsNotFound_WhenUserIdIsNullOrEmpty()
+        {
+            var email = "123@123.com";
+            GetEmailResetCodeResult getEmailResetCodeResult =
+                new GetEmailResetCodeResult(IdentityResult.Failed(), email, "Subject", "Message");
+            ResetPasswordByEmailResetCodeEvent resetPasswordByEmailResetCodeEvent =
+                new ResetPasswordByEmailResetCodeEvent(getEmailResetCodeResult.Email, getEmailResetCodeResult.Subject, getEmailResetCodeResult.HtmlMessage);
+            userDomainServiceMock.Setup(ud => ud.GetEmailResetCode(It.IsAny<string>())).ReturnsAsync(getEmailResetCodeResult);
+
+
+            var result = await userController.GetEmailResetCode(email);
+
+
+            userDomainServiceMock.Verify(ud => ud.GetEmailResetCode(email));
+            var notFoundObjectResult = Assert.IsType<NotFoundObjectResult>(result);
+            notFoundObjectResult.StatusCode.Should().Be(404);
+
+        }
+
+        [Fact]
+        public async Task GetUserInfo_ReturnsNotFound_WhenUserIdIsNullOrEmpty()
+        {
+            var user = new ClaimsPrincipal(new ClaimsIdentity(
+        [
+                               new Claim(ClaimTypes.NameIdentifier, "")
+                         ]));
+
+            userController.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = user }
+            };
+
+            var result = await userController.GetUserInfo();
+
+            var notFoundObjectResult = Assert.IsType<NotFoundObjectResult>(result.Result);
+            notFoundObjectResult.StatusCode.Should().Be(404);
+        }
+
+        [Fact]
+        public async Task GetUserInfo_ReturnsNotFound_WhenUserIsNotFound()
+        {
+            var userId = Guid.NewGuid();
+            var user = new ClaimsPrincipal(new ClaimsIdentity(
+            [
+            new Claim(ClaimTypes.NameIdentifier, userId.ToString())
+            ]));
+
+            userController.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = user }
+            };
+
+
+            userRepositoryMock.Setup(repo => repo.FindByIdAsync(It.IsAny<UserId>()))
+                                 .ReturnsAsync((User)null!);
+
+            var result = await userController.GetUserInfo();
+
+
+            userRepositoryMock.Verify(repo => repo.FindByIdAsync(It.Is<UserId>(id => id.Value == userId)), Times.Once);
+            var notFoundObjectResult = Assert.IsType<NotFoundObjectResult>(result.Result);
+            notFoundObjectResult.StatusCode.Should().Be(404);
+
+        }
+
+        [Fact]
+        public async Task GetUserInfo_ReturnsOk_WhenUserIsFound()
+        {
+            var userMock = new User("user", "123123@qwe.com");
+            var user = new ClaimsPrincipal(new ClaimsIdentity(
+            [
+            new Claim(ClaimTypes.NameIdentifier, userMock.Id.ToString())
+            ]));
+
+            userController.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = user }
+            };
+
+
+            userRepositoryMock.Setup(repo => repo.FindByIdAsync(It.IsAny<UserId>()))
+                                 .ReturnsAsync(userMock);
+
+            var result = await userController.GetUserInfo();
+
+
+            userRepositoryMock.Verify(repo => repo.FindByIdAsync(It.Is<UserId>(id => id == userMock.Id)), Times.Once);
+            result.Result.Should().BeNull();
+            var userInfo = Assert.IsType<GetUserInfoResponse>(result.Value);
+            userInfo.Id.Should().Be(userMock.Id);
+            userInfo.Name.Should().Be(userMock.UserName);
+
+
+        }
+
+
+
+        [Fact]
+        public async Task LoginByEmailAndPassword_ReturnsToken_WhenLoginIsSuccessful()
+        {
+            // Arrange
+            var loginRequest = new LoginByEmailAndPasswordRequest(
+            
+                "test@example.com",
+                "Password123"
+            );
+            var loginResult = new LoginResult
+            {
+                SignInResult = Microsoft.AspNetCore.Identity.SignInResult.Success,
+                Token = "some-jwt-token"
+            };
+
+            userDomainServiceMock.Setup(service => service.LoginByEmailAndPasswordAsync(It.IsAny<string>(), It.IsAny<string>()))
+                                                                                                  .ReturnsAsync(loginResult);
+
+            // Act
+            var result = await userController.LoginByEmailAndPassword(loginRequest);
+
+            // Assert
+            userDomainServiceMock.Verify(service => service.LoginByEmailAndPasswordAsync(loginRequest.Email, loginRequest.Password));
+            result.Result.Should().BeNull();
+            result.Value.Should().Be(loginResult.Token);
+
+        }
+        [Fact]
+        public async Task LoginByEmailAndPassword_ReturnsLocked_WhenAccountIsLocked()
+        {
+            // Arrange
+            var loginRequest = new LoginByEmailAndPasswordRequest(
+
+                 "test@example.com",
+                 "Password123"
+             );
+
+            var loginResult = new LoginResult
+            {
+                SignInResult = Microsoft.AspNetCore.Identity.SignInResult.LockedOut,
+                Token = "some-jwt-token"
+            };
+
+            userDomainServiceMock.Setup(service => service.LoginByEmailAndPasswordAsync(It.IsAny<string>(), It.IsAny<string>()))
+                                                .ReturnsAsync(loginResult);
+
+            // Act
+            var result = await userController.LoginByEmailAndPassword(loginRequest);
+
+            // Assert
+            userDomainServiceMock.Verify(service => service.LoginByEmailAndPasswordAsync(loginRequest.Email, loginRequest.Password));
+            var lockedResult = Assert.IsType<ObjectResult>(result.Result);
+            lockedResult.StatusCode.Should().Be((int)HttpStatusCode.Locked);  
+        }
+
+        [Fact]
+        public async Task LoginByEmailAndPassword_ReturnsUnauthorized_WhenLoginFails()
+        {
+            // Arrange
+            var loginRequest = new LoginByEmailAndPasswordRequest(
+
+               "test@example.com",
+               "Password123"
+           );
+
+            var loginResult = new LoginResult
+            {
+                SignInResult = Microsoft.AspNetCore.Identity.SignInResult.Failed,
+                Token = "some-jwt-token"
+            };
+
+            userDomainServiceMock.Setup(service => service.LoginByEmailAndPasswordAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(loginResult);
+
+            // Act
+            var result = await userController.LoginByEmailAndPassword(loginRequest);
+
+            // Assert
+            userDomainServiceMock.Verify(service => service.LoginByEmailAndPasswordAsync(loginRequest.Email, loginRequest.Password));
+            var unauthorizedResult = Assert.IsType<UnauthorizedObjectResult>(result.Result);
+            unauthorizedResult.StatusCode.Should().Be((int)HttpStatusCode.Unauthorized);
+
+        }
+
+
+        [Fact]
+        public async Task LoginByNameAndPassword_ReturnsToken_WhenLoginIsSuccessful()
+        {
+            // Arrange
+            var loginRequest = new LoginByNameAndPasswordRequest(
+
+                "name",
+                "Password123"
+            );
+            var loginResult = new LoginResult
+            {
+                SignInResult = Microsoft.AspNetCore.Identity.SignInResult.Success,
+                Token = "some-jwt-token"
+            };
+
+            userDomainServiceMock.Setup(service => service.LoginByNameAndPasswordAsync(It.IsAny<string>(), It.IsAny<string>()))
+                                                                                                  .ReturnsAsync(loginResult);
+
+            // Act
+            var result = await userController.LoginByNameAndPassword(loginRequest);
+
+            // Assert
+            userDomainServiceMock.Verify(service => service.LoginByNameAndPasswordAsync(loginRequest.Name, loginRequest.Password));
+            result.Result.Should().BeNull();
+            result.Value.Should().Be(loginResult.Token);
+
+        }
+
+
+        [Fact]
+        public async Task LoginByNameAndPassword_ReturnsLocked_WhenAccountIsLocked()
+        {
+            // Arrange
+            var loginRequest = new LoginByNameAndPasswordRequest(
+
+                "name",
+                "Password123"
+            );
+
+            var loginResult = new LoginResult
+            {
+                SignInResult = Microsoft.AspNetCore.Identity.SignInResult.LockedOut,
+                Token = "some-jwt-token"
+            };
+
+            userDomainServiceMock.Setup(service => service.LoginByNameAndPasswordAsync(It.IsAny<string>(), It.IsAny<string>()))
+                                                                                                  .ReturnsAsync(loginResult);
+
+            // Act
+            var result = await userController.LoginByNameAndPassword(loginRequest);
+
+            // Assert
+            userDomainServiceMock.Verify(service => service.LoginByNameAndPasswordAsync(loginRequest.Name, loginRequest.Password));
+            var lockedResult = Assert.IsType<ObjectResult>(result.Result);
+            lockedResult.StatusCode.Should().Be((int)HttpStatusCode.Locked);
+        }
+
+
+        [Fact]
+        public async Task LoginByNameAndPassword_ReturnsUnauthorized_WhenLoginFails()
+        {
+            // Arrange
+            var loginRequest = new LoginByNameAndPasswordRequest(
+
+                "name",
+                "Password123"
+            );
+
+            var loginResult = new LoginResult
+            {
+                SignInResult = Microsoft.AspNetCore.Identity.SignInResult.Failed,
+                Token = "some-jwt-token"
+            };
+
+            userDomainServiceMock.Setup(service => service.LoginByNameAndPasswordAsync(It.IsAny<string>(), It.IsAny<string>()))
+                                                                                                  .ReturnsAsync(loginResult);
+
+            // Act
+            var result = await userController.LoginByNameAndPassword(loginRequest);
+
+            // Assert
+            userDomainServiceMock.Verify(service => service.LoginByNameAndPasswordAsync(loginRequest.Name, loginRequest.Password));
+            var unauthorizedResult = Assert.IsType<UnauthorizedObjectResult>(result.Result);
+            unauthorizedResult.StatusCode.Should().Be((int)HttpStatusCode.Unauthorized);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
